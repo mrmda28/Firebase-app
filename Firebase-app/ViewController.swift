39cee6562db1a2977a8a0172c3ed1f802e6a886d
mrmda28 <mrmda28@gmail.com>
2021-11-27 13:43:22 +0300

    Initial commit

---
+++ Firebase-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-app
+//
+//  Created by Dmitriy Maslennikov on 27/11/2021.
+//  Copyright © 2021 Dmitriy Maslennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
